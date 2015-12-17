f9a5d9d06a26d48eebe22066d2185e7b862e9c15
mitch <mitch@repositpower.com>
2015-12-17 13:24:26 +1100

    fixed lanes import in app

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,6 @@
 import AltContainer from 'alt-container';
 import React from 'react';
-import Lanes from './Lanes.jsx';
+import Lanes from './Lanes';
 import LaneActions from '../actions/LaneActions';
 import LaneStore from '../stores/LaneStore';
 import {DragDropContext} from 'react-dnd';
