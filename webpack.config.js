bff9e33f10785e039c20f191ac0d4af921292cb9
mitch <mitch@repositpower.com>
2015-12-19 18:00:46 +1100

    inital tests

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ var pkg = require('./package.json');
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
+  test: path.join(__dirname, 'tests')
 };
 
 process.env.BABEL_ENV = TARGET;
@@ -128,5 +129,33 @@ if(TARGET === 'build' || TARGET === 'stats') {
         }
       })
     ]
+  });
+}
+if(TARGET === 'test' || TARGET === 'tdd') {
+  module.exports = merge(common, {
+    entry: {}, // karma will set this
+    output: {}, // and this
+    devtool: 'inline-source-map',
+    resolve: {
+      alias: {
+        'app': PATHS.app
+      }
+    },
+    module: {
+      preLoaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['isparta-instrumenter'],
+          include: PATHS.app
+        }
+      ],
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['babel'],
+          include: PATHS.test
+        }
+      ]
+    }
   });
 }
